f52e344804241b9f0b6871e06115a3e356058fc1
Jamie Flynt <jamflynt@gmail.com>
2021-09-23 11:49:02 -0400

    Added comments. Did not change answers.

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let skullBones=`I don't have a skull ${dotDotDot} or bones.`;
 console.log(Math.PI);
 // 5
 let randomNumber=Math.random()*3;
-randomNumber=Math.ceil(randomNumber)
+randomNumber=Math.ceil(randomNumber) // Add Math.floor and randomNum++; could possibly get zero with ciel
 console.log(randomNumber);
 
 // Bonus
@@ -28,11 +28,12 @@ console.log(` Let it Go!`.trim().repeat(2).toUpperCase());
 let reindeers="Reindeers are better than people."
 // 7b --> Probably a better way to do this
 reindeers=reindeers.replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_")
+// change to console.log(reindeers.replace(/ /g, "_")); the "g" stands for global meaning all in string
 console.log(reindeers);
 
 // 8
 console.log(Math.sqrt(2));
 
 // 9
-let newRandomNumber=Math.floor(Math.random()*(17))+7;
+let newRandomNumber=Math.floor(Math.random()*(17))+7; // Did my +7 outside of the everything and still got it to work?
 console.log(newRandomNumber);
